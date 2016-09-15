8983b0ecb8ccb3ef61cf520c5f14d55a0101b372
Shayne <Shayne@SORBITAL-SURF>
2016-09-15 13:32:02 -0400

    Began defining the Container class. Should be ~(0% Defined.

---
+++ SOSQL/Container.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOSQL
+{
+    public class Container
+    {
+        private List<float> InternalDimensions;
+        private List<float> ExternalDimensions;
+        private float WeightLimit;
+        private float Weight;
+        private bool RequiresBalance;
+
+        public Container(List<float> _InternalDimensions, List<float> _ExternalDimensions, float _WeightLimit,
+            float _Weight, bool _RequiresBalance)
+        {
+            InternalDimensions = _InternalDimensions;
+            ExternalDimensions = _ExternalDimensions;
+            WeightLimit = _WeightLimit;
+            Weight = _Weight;
+            RequiresBalance = _RequiresBalance;
+        }
+    }
+}
