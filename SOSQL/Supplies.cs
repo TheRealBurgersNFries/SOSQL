bf123d6aa3e8de5db7186a643f0b774dab957403
Shayne <Shayne@SORBITAL-SURF>
2016-10-04 11:13:14 -0400

    Made changes to Supplies to make all of that work as desired

--- SOSQL/Supplies.cs
+++ SOSQL/Supplies.cs
@@ -9,22 +9,22 @@ namespace SOSQL
     public class Supply
     {
         private string Name;
-        private int Box;
+        private int BoxNumber;
         private float Weight;
-        private int Count;
         private float Value;
         private float TotalValue;
+        private int Count;
         private string Pallet;
         private string BoxType;
     
-        public Supply (string _Name, int _Box, float _Weight, int _Count, float _Value, string _Pallet,
-            string _BoxType)
+        public Supply (string _Name, int _BoxNumber, float _Weight, float _Value, int _Count,
+            string _Pallet, string _BoxType)
         {
             Name = _Name;
-            Box = _Box;
+            BoxNumber = _BoxNumber;
             Weight = _Weight;
-            Count = _Count;
             Value = _Value;
+            Count = _Count;
             TotalValue = Count * Value;
             Pallet = _Pallet;
             BoxType = _BoxType;
